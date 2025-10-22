11a762b896ada3e8047a302156fd0db6ce5b0ff7
New-Sohan <mrsohansadhukhan@gmail.com>
2025-10-22 19:18:45 +0530

    add LoginForm component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
+import LoginForm from "@/components/LoginForm";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+} from "@/components/shadcnui/card";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Home | Nextjs Starter Template",
-	description: "Home page of Nextjs Starter Template",
+	title: "Login | RHF Auth Form",
+	description: "Login page of RHF Auth Form",
 };
 
 const page = () => {
 	return (
-		<>
-			<section className="grid h-[80dvh] place-items-center"></section>
-		</>
+		<section className="grid h-[80dvh] place-items-center">
+			<Card className="w-xs">
+				<CardHeader>
+					<CardTitle className="text-center text-2xl">
+						Login Form
+					</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<LoginForm />
+				</CardContent>
+			</Card>
+		</section>
 	);
 };
 
